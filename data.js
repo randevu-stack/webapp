0b23ae4cef41f599a90e518737f13ed0231cf9cf
randevu-stack <khan.maverick@bk.ru>
2026-05-02 17:41:59 +0500

    Add files via upload

--- data.js
+++ data.js
@@ -1,12 +1,82 @@
 const drugs = [
-  {
+{
+    name: "Торасса",
+    indications: "отёки, сердечная недостаточность",
+    photo: "images/torassa.webp"
+},
+{
+    name: "Ларгин",
+    indications: "сосудистые нарушения, кровообращение",
+    photo: "images/largin.webp"
+},
+{
+    name: "Карблок",
+    indications: "давление, гипертония",
+    photo: "images/carblock.webp"
+},
+{
+    name: "Добутамин",
+    indications: "сердечная недостаточность, шок",
+    photo: "images/dobutamin.webp"
+},
+{
     name: "Амиодарон",
     indications: "аритмия, тахикардия",
-    photo: "https://via.placeholder.com/150"
-  },
-  {
-    name: "Карблок",
-    indications: "давление",
-    photo: "https://via.placeholder.com/150"
-  }
+    photo: "images/amiodaron.webp"
+},
+{
+    name: "Энерфос",
+    indications: "энергетическая поддержка, метаболизм",
+    photo: "images/enerfos.webp"
+},
+{
+    name: "Оксиум",
+    indications: "гипоксия, дыхательная поддержка",
+    photo: "images/oxium.webp"
+},
+{
+    name: "Каспаргин (инфузионный)",
+    indications: "дефицит калия и магния",
+    photo: "images/kaspargin1.webp"
+},
+{
+    name: "Каспаргин (концентрат)",
+    indications: "нарушение электролитов",
+    photo: "images/kaspargin2.webp"
+},
+{
+    name: "Кромостаб",
+    indications: "аллергия, астма",
+    photo: "images/kromostab.webp"
+},
+{
+    name: "Нубетал",
+    indications: "боль, воспаление",
+    photo: "images/nubetal.webp"
+},
+{
+    name: "Изивент Дуо",
+    indications: "бронхиальная астма, дыхание",
+    photo: "images/izivent_duo.webp"
+},
+{
+    name: "Изивент",
+    indications: "бронхоспазм",
+    photo: "images/izivent.webp"
+},
+{
+    name: "Ликвин",
+    indications: "детоксикация",
+    photo: "images/liquin.webp"
+},
+{
+    name: "Буфесал 3 Гиал",
+    indications: "суставы, воспаление",
+    photo: "images/bufesal3.webp"
+},
+{
+    name: "Буфесал 7 Гиал",
+    indications: "суставы, регенерация",
+    photo: "images/bufesal7.webp"
+}
 ];
